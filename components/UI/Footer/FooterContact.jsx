8e62bf5cc76e4617c92604626ba7008819603243
dhoat30 <dhoat30@gmail.com>
2025-03-18 10:46:14 +1300

    first commit

---
+++ components/UI/Footer/FooterContact.jsx
@@ -0,0 +1,20 @@
+export const contactInfo = { 
+    info:[ 
+        {
+            url: "tel:0450 033 171", 
+            label: "0450 033 171",
+            icon: { 
+                url: "/phone.png", 
+                alt: "phone icon" 
+            },
+        },
+        {
+            url: "mailto:liftandshiftmoversau@gmail.com", 
+            label: "liftandshiftmoversau@gmail.com", 
+            icon: { 
+                url: "/email.png", 
+                alt: "Email icon" 
+            },
+        }
+    ]
+}
